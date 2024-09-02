c2d2e808fa436fc9f709b03c4f7102220a1f63af
hoang_anh <ledinhhoanganh10@gmail.com>
2024-09-02 22:40:09 +0700

    Base Layout

---
+++ src/routes/index.js
@@ -0,0 +1,39 @@
+//Layout
+import { HeaderOnly } from '~/components/Layout';
+
+//Pages
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+import Profile from '~/pages/Profile';
+
+//Public Routes
+const publicRoutes = [
+   {
+      path: '/',
+      component: Home,
+   },
+   {
+      path: '/following',
+      component: Following,
+   },
+   {
+      path: '/upload',
+      component: Upload,
+      layout: HeaderOnly,
+   },
+   {
+      path: '/profile',
+      component: Profile,
+   },
+   {
+      path: '/search',
+      component: Search,
+      layout: null,
+   },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
