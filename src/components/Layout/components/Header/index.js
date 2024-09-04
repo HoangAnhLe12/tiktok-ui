e81e54376c818837fc05f436df34ca2ef1327e6a
hoang_anh <ledinhhoanganh10@gmail.com>
2024-09-04 23:32:41 +0700

    Header part 2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,16 +1,55 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import {
+   faCircleXmark,
+   faSpinner,
+   faMagnifyingGlass,
+   faEllipsisVertical,
+   faLanguage,
+   faCircleQuestion,
+   faKeyboard,
+} from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
 
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
+import { Menu } from '~/components/Popper';
+
 import AccountItem from '~/components/AccountItem';
+import Button from '~/components/Button';
 
 const cx = classNames.bind(styles);
+const MENU_ITEM = [
+   {
+      icon: <FontAwesomeIcon icon={faLanguage} />,
+      title: 'English',
+      children: {
+         title: 'Language',
+         data: [
+            {
+               code: 'en',
+               title: 'English',
+            },
+            {
+               code: 'vi',
+               title: 'Vietnamese',
+            },
+         ],
+      },
+   },
+   {
+      icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+      title: 'Feedback and Help',
+      to: '/feedback',
+   },
+   {
+      icon: <FontAwesomeIcon icon={faKeyboard} />,
+      title: 'Keyboard Shortcuts',
+   },
+];
 
 function Header() {
    const [searchResult, setSearhResult] = useState([]);
@@ -20,6 +59,10 @@ function Header() {
          setSearhResult([]);
       }, 0);
    }, []);
+
+   const handleMenuChange = (menuItem) => {
+      console.log(menuItem);
+   };
 
    return (
       <header className={cx('wrapper')}>
@@ -45,14 +88,22 @@ function Header() {
                   <button className={cx('clear')}>
                      <FontAwesomeIcon icon={faCircleXmark} />
                   </button>
-                  {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+                  <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
 
                   <button className={cx('search-btn')}>
                      <FontAwesomeIcon icon={faMagnifyingGlass} />
                   </button>
                </div>
             </Tippy>
-            <div className={cx('action')}>Action</div>
+            <div className={cx('action')}>
+               <Button text>Upload</Button>
+               <Button primary>Login</Button>
+               <Menu items={MENU_ITEM} onChange={handleMenuChange}>
+                  <button className={cx('more-btn')}>
+                     <FontAwesomeIcon icon={faEllipsisVertical} />
+                  </button>
+               </Menu>
+            </div>
          </div>
       </header>
    );
